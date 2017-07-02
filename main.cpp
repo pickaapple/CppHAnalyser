b340d1435dd313bbfc8e0b5f62e199b53459c8cd
STrawberryer <646790420@qq.com>
2017-07-03 00:35:48 +0800

    empty

--- main.cpp
+++ main.cpp
@@ -5,17 +5,14 @@
 //  Created by yhys on 2017/6/4.
 //  Copyright © 2017年 STRAWBERRY STUDIO. All rights reserved.
 //
-#include "List.hpp"
+//#include "List.hpp"
 #include "StateChart.hpp"
 
 #include <stdio.h>
 
 using namespace st;
+
 int main(int argc, const char * argv[]) {
-    DigitChart chart1;
-    utf8::uchar testChar1 = '0';
-    utf8::uchar testChar2 = 'x';
-    printf("%s pass 0\n",chart1.Pass(testChar1)?"":"dont");
-    printf("%s pass x\n",chart1.Pass(testChar2)?"":"dont");
+    
     return 0;
 }
