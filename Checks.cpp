b9bc3d22ea81b7384806530489df84efd10dc956
STrawberryer <646790420@qq.com>
2017-07-03 00:28:13 +0800

    ASSERT(cmd)
    simple ASSERT define

---
+++ Checks.cpp
@@ -0,0 +1,9 @@
+//
+//  Checks.cpp
+//  CppAnalyser
+//
+//  Created by yhys on 2017/7/2.
+//  Copyright © 2017年 STRAWBERRY STUDIO. All rights reserved.
+//
+
+#include "Checks.hpp"
---
+++ Checks.hpp
@@ -0,0 +1,21 @@
+//
+//  Checks.hpp
+//  CppAnalyser
+//
+//  Created by yhys on 2017/7/2.
+//  Copyright © 2017年 STRAWBERRY STUDIO. All rights reserved.
+//
+
+#ifndef Checks_hpp
+#define Checks_hpp
+
+#include "platform.hpp"
+
+#define ASSERT(cmd) do{ if(!(cmd)) OS::Abort(); } while(0)
+
+
+namespace st {
+
+}
+
+#endif /* Checks_hpp */
