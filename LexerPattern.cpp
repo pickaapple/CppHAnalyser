923ee2530c5fffa7c2144bb538f8372620475b7e
STrawberryer <646790420@qq.com>
2017-07-09 23:18:16 +0800

    Lexer pattern for
    id;{id};_sName1
    num;{num};123
    namespace;{namespace};namespace
    class;{class};class
    struct;{struct};struct
    visibility;{visibility};public
    variable type;{variable type};void
    variable sign;{variable sign};unsigned
    variable flag;{variable flag};&
    const;{const};const
    static;{static};static
    symbol;{symbol};:

---
+++ LexerPattern.cpp
@@ -0,0 +1,9 @@
+//
+//  LexerPattern.cpp
+//  CppAnalyser
+//
+//  Created by yhys on 2017/7/3.
+//  Copyright © 2017年 STRAWBERRY STUDIO. All rights reserved.
+//
+
+#include "LexerPattern.hpp"
---
+++ LexerPattern.hpp
@@ -0,0 +1,16 @@
+//
+//  LexerPattern.hpp
+//  CppAnalyser
+//
+//  Created by yhys on 2017/7/3.
+//  Copyright © 2017年 STRAWBERRY STUDIO. All rights reserved.
+//
+
+#ifndef LexerPattern_hpp
+#define LexerPattern_hpp
+
+#include "Macro.h"
+
+
+
+#endif /* LexerPattern_hpp */
