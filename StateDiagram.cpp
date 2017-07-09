ff669c22b326e4e2cd9d43fa1ea223660847972f
STrawberryer <646790420@qq.com>
2017-07-09 23:25:48 +0800

    diagram of chart

---
+++ StateDiagram.cpp
@@ -0,0 +1,9 @@
+//
+//  StateDiagram.cpp
+//  CppAnalyser
+//
+//  Created by yhys on 2017/7/3.
+//  Copyright © 2017年 STRAWBERRY STUDIO. All rights reserved.
+//
+
+#include "StateDiagram.hpp"
---
+++ StateDiagram.hpp
@@ -0,0 +1,25 @@
+//
+//  StateDiagram.hpp
+//  CppAnalyser
+//
+//  Created by yhys on 2017/7/3.
+//  Copyright © 2017年 STRAWBERRY STUDIO. All rights reserved.
+//
+
+#ifndef StateDiagram_hpp
+#define StateDiagram_hpp
+
+#include "Macro.h"
+#include "List.hpp"
+#include "StateChart.hpp"
+namespace st{
+    class StateDiagram
+    {
+    public:
+        StateDiagram();
+        virtual ~StateDiagram();
+    protected:
+        List<BaseChart*> _charts;
+    };
+}
+#endif /* StateDiagram_hpp */
