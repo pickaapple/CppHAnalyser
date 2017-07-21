0c40e737a71939d0e01323a6f1e8d0e6dac118e2
STrawberryer_at_com <646790420@qq.com>
2017-07-21 18:43:58 +0800

    tree

--- Tree.cpp
+++ Tree.cpp
@@ -8,30 +8,5 @@
 
 #include "Tree.hpp"
 namespace st{
-    template<class P>
-    bool BinaryTreeNode<P>::operator < (const BinaryTreeNode<P> &node) const {
-        return _p < node._p;
-    }
-    template<class P>
-    bool BinaryTreeNode<P>::operator > (const BinaryTreeNode<P> &node) const {
-        return _p > node._p;
-    }
-    template<class P>
-    bool BinaryTreeNode<P>::operator == (const BinaryTreeNode<P> &node) const {
-        return _p == node._p;
-    }
-    template<class P>
-    BinaryTreeNode<P>* BinaryTreeNode<P>::NextEmpty(){
-        if(!_left){
-            return _left;
-        }
-        if(!_right){
-            return _right;
-        }
-        return nullptr;
-    }
-    template<class N,class P>
-    void Tree<N,P>::InjectNodes(const payload_type* nodes,const payload_type& endOfNode){
-        
-    }
+   
 }
--- Tree.hpp
+++ Tree.hpp
@@ -9,34 +9,139 @@
 #ifndef Tree_hpp
 #define Tree_hpp
 #include "Allocation.hpp"
-namespace st{
-    template<class P>
-    class BinaryTreeNode{
+namespace st
+{
+    template<class P /*Payload*/>
+    class BinaryTreeNode
+	{
+	public:
         typedef P payload_type;
-        BinaryTreeNode* _parent;
-        BinaryTreeNode* _left;
-        BinaryTreeNode* _right;
-        payload_type _p;
-        /**
-         Next empty node of this node
 
-         @return 
-            next custom node's address, but null when this node has full children.
-         */
-        BinaryTreeNode<P>* NextEmpty();
-        bool operator ==    (const BinaryTreeNode& node) const;
-        bool operator >     (const BinaryTreeNode& node) const;
-        bool operator <     (const BinaryTreeNode& node) const;
+		BinaryTreeNode<P>* GetFirstChild();
+
+		BinaryTreeNode<P>* GetRightBrother();
+
+		bool AddChild(BinaryTreeNode<P>* node);
+
+		BinaryTreeNode();
+
+		virtual ~BinaryTreeNode();
+
+		bool operator ==    (const BinaryTreeNode& node) const;
+
+		bool operator ==    (const payload_type& payload) const;
+
+		bool operator >     (const BinaryTreeNode& node) const;
+
+		bool operator >     (const payload_type& payload) const;
+
+		bool operator <     (const BinaryTreeNode& node) const;
+
+		bool operator <     (const payload_type& payload) const;
+
+	public:
+		BinaryTreeNode* _parent;
+		BinaryTreeNode* _left;
+		BinaryTreeNode* _right;
+		payload_type _p;
     };
-    template<class N,class P = Mallocation>
-    class Tree{
+
+
+	template<class P /*Payload*/>
+	st::BinaryTreeNode<P>::BinaryTreeNode()
+	{
+
+	}
+
+	template<class P /*Payload*/>
+	st::BinaryTreeNode<P>::~BinaryTreeNode()
+	{
+
+	}
+
+	template<class P /*Payload*/>
+	bool BinaryTreeNode<P>::operator < (const BinaryTreeNode<P> &node) const 
+	{
+		return _p < node._p;
+	}
+
+	template<class P /*Payload*/>
+	bool BinaryTreeNode<P>::operator < (const payload_type& payload) const
+	{
+		return _p < payload;
+	}
+
+	template<class P /*Payload*/>
+	bool BinaryTreeNode<P>::operator > (const BinaryTreeNode<P> &node) const 
+	{
+		return _p > node._p;
+	}
+
+	template<class P /*Payload*/>
+	bool BinaryTreeNode<P>::operator > (const payload_type& payload) const
+	{
+		return _p > payload;
+	}
+
+	template<class P /*Payload*/>
+	bool BinaryTreeNode<P>::operator == (const BinaryTreeNode<P> &node) const 
+	{
+		return _p == node._p;
+	}
+
+	template<class P /*Payload*/>
+	bool BinaryTreeNode<P>::operator==(const payload_type& payload) const
+	{
+		return _p = payload;
+	}
+
+
+	//////////////////////////////////////////////////////////////////////////
+
+    template<class P /*Payload*/, class N /*Node*/ = BinaryTreeNode<P>,class MA /*Memory Allocation*/= Mallocation>
+    class Tree
+	{
     public:
-        typedef N node_type;
-        typedef P payload_type;
-        void InjectNodes(const payload_type* nodes,const payload_type& endOfNode);
-        node_type root;
-    };
-    
-    typedef Tree<BinaryTreeNode<char>> BTreeOfChar;
+        typedef N	node_type;
+        typedef MA	mallocation_type;
+		typedef P	payload_type;
+
+        void InjectNodes(const payload_type* nodes,size_t length);
+
+	protected:
+		node_type _root;
+	};
+
+	typedef Tree<char> BTreeOfChar;
+
+	template<class P /*Payload*/, class N /*Node*/ /*= BinaryTreeNode<P>*/, class MA /*Memory Allocation*//*= Mallocation*/>
+	void Tree<P, N, MA>::InjectNodes(const payload_type* nodes, size_t length)
+	{
+		int i;
+		foreachArray(i, length)
+		{
+			const payload_type& currentPayload = *(nodes + i);
+			node_type* currentNode = _root->GetFirstChild();
+			do 
+			{
+				if (currentNode == currentPayload) 
+				{
+					currentNode = currentNode->GetFirstChild();
+					if (!currentNode) 
+					{
+						currentNode->AddChild(currentPayload);
+						break;
+					}
+				}
+
+				currentNode = currentNode->GetRightBrother();
+				if (!currentNode) 
+				{
+					currentNode->_p.AddChild(currentPayload);
+					break;
+				}
+			} while (1);
+		}
+	}
 }
 #endif /* Tree_hpp */
